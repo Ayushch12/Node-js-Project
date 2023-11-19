7801ec554105423cb0bf12d3ed7090bc5e30eb49
Ayush chalisee <ayushchalise12@gmail.com>
2023-11-19 23:04:10 +0100

    Config : Create a file dbconnection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorhandler");
+const { connect } = require("http2");
 const dotenv = require("dotenv").config();
+const connectDb = require("./config/dbConnection")
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 4000;
