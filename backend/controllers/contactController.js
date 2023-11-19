60d4a5d3350eff9d96ea5aba0e087dee1e7f6b2a
Ayush chalisee <ayushchalise12@gmail.com>
2023-11-19 18:14:04 +0100

    Added constants: for handle the error

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -11,16 +11,20 @@ const getContacts = (req, res) => {
 //@access public
 const createContact = (req, res) => {
   console.log("The request body is : ", req.body);
+  const {name, email, phone} = req.body;
+  if(!name || !email || !phone){
+    res.status(400);
+    throw new Error("All fields are mandatory")
+  }
+
   res.status(201).json({ message: "Create Contact" });
 };
-
 //@desc Get contact by ID
 //@route GET/api/contacts/:id
 //@access public
 const getContact = (req, res) => {
   res.status(200).json({ message: `Get contact for ${req.params.id}` });
 };
-
 //@desc Update contacts
 //@route PUT/api/contacts/:id
 //@access public
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorhandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
@@ -6,8 +7,8 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());       //Build middleware for post request
-app.use("/api/contacts", require("./routes/contactRoutes"
-));
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
