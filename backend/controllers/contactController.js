20d6c33f0f0e60baf840419a1af889cc78ae39b8
Ayush chalisee <ayushchalise12@gmail.com>
2023-11-19 17:33:29 +0100

    Fetching date from create

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,39 +1,44 @@
+
 //@desc Get all contacts
 //@route GET/api/contacts
 //@access public
-const getContacts = (req,res)=>{
-    res.status(200).json({ message: "Get all the messages"})
-  };
+const getContacts = (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+};
 
-  //@desc Create new contacts
+//@desc Create new contacts
 //@route POST/api/contacts
 //@access public
-const CreateContact = (req,res)=>{
-    res.status(200).json({ message: "Get all the messages"})
-  };
+const createContact = (req, res) => {
+  console.log("The request body is : ", req.body);
+  res.status(201).json({ message: "Create Contact" });
+};
 
-//@desc Get all contacts
+//@desc Get contact by ID
 //@route GET/api/contacts/:id
 //@access public
-const getContact = (req,res)=>{
-    res.status(200).json({ message: `Update contact for ${req.params.id}`})
-  };
+const getContact = (req, res) => {
+  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+};
 
-  //@desc updatel contacts
-//@route update/api/contacts/:id
+//@desc Update contacts
+//@route PUT/api/contacts/:id
 //@access public
-const updateContact = (req,res)=>{
-    res.status(200).json({ message: `Update contact for ${req.params.id}`})
-  };
+const updateContact = (req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+};
 
-  //@desc delete contacts
-//@route delete/api/contacts:id
+//@desc Delete contacts
+//@route DELETE/api/contacts/:id
 //@access public
-const deleteContact = (req,res)=>{
-    res.status(200).json({ message: `Delete contact for ${req.params.id}`})
-  };
+const deleteContact = (req, res) => {
+  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+};
 
-
-  module.exports = {getContacts,getContact,CreateContact,deleteContact,updateContact } ;
-
-
+module.exports = {
+  getContacts,
+  getContact,
+  createContact,
+  deleteContact,
+  updateContact,
+};
--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,24 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const {getContact,
-    getContacts,
-    CreateContact,
-    deleteContact,
-    updateContact} = require("../controllers/contactController")
+const {
+  getContacts,
+  createContact,
+  getContact,
+  deleteContact,
+  updateContact,
+} = require("../controllers/contactController");
 
 //Define a get method firn all contacts-------
-router.route("/").get(getContacts);
-
-//Define a POST method -------
-router.route("/").post( CreateContact);
+router.route("/").get(getContacts).post(createContact);
 
 //Define a GET:id methodd -------
-  router.route("/:id").get(  getContact);
-
-  //Define a PUT:id method ----------------
-router.route("/:id").put( updateContact);
-
-  //Define a DELETE:id method -------
-router.route("/:id").delete(deleteContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
+
+
