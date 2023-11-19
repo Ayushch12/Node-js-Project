d2b62c85b9120888653e4e734747a0cc05ffbab1
Ayush chalisee <ayushchalise12@gmail.com>
2023-11-19 23:32:23 +0100

    Controller: Updated with new model data name,email and phone

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -8,19 +8,23 @@ const getContacts = asyncHandler(async(req, res) => {
   const contacts = await Contact.find();     // Thsi is comming from the contactModel
   res.status(200).json( contacts);
 });
-
 //@desc Create new contacts
 //@route POST/api/contacts
 //@access public
+
 const createContact = asyncHandler(async(req, res) => {
   console.log("The request body is : ", req.body);
   const {name, email, phone} = req.body;
   if(!name || !email || !phone){
     res.status(400);
-    throw new Error("All fields are mandatory")
+    throw new Error("All fields are mandatory");
   }
-
-  res.status(201).json({ message: "Create Contact" });
+  const contact = await Contact.create({
+    name,
+    email,
+    phone
+  });
+  res.status(201).json(contact);
 });
 //@desc Get contact by ID
 //@route GET/api/contacts/:id
