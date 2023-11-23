3afffd843a08c8562e5e1c3313927231261dedbe
Ayush chalisee <ayushchalise12@gmail.com>
2023-11-23 22:34:51 +0100

    Usersroutes.js: Created a post CRUD

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());       //Build middleware for post request
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
