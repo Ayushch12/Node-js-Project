fa8a0988899bb4e4516fabdcde433c97d956b9b5
Ayush chalisee <ayushchalise12@gmail.com>
2023-11-23 23:24:47 +0100

    userControllers : Updated with install bcyrpt  (for hash)

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,9 +1,22 @@
 const asyncHandler = require("express-async-handler") ;
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel")
 
 //@desc Register a user
 //@route POST/api/users/register
 //@access public
 const registerUser = asyncHandler(async(req, res) => {
+    const {username,email,password} = req.body;
+    if(!username || !email || password ){
+        res.status(400);
+        throw new ErrorTwoTone("All field are mandatory!");
+    }
+    const userAvailable = await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already register!");
+    }
+
   res.json({message: "Register the user"})
   });
 
@@ -15,12 +28,12 @@ const loginUser = asyncHandler(async (req, res)=>{
 });
 
     //@desc Currnet user info
-//@route POST/api/users/register
+//@route PUT/api/users/current
 //@access private
 const currentUser = asyncHandler(async(req, res)=>{
     res.json({message: "Current user information"});
 });
 
-  module.exports= {registerUser,loginUser,currentUser   };
+  module.exports= {registerUser,loginUser,currentUser};
 
 
