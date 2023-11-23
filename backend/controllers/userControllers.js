71082a366be016c102c840bed384c380a6b1408e
Ayush chalisee <ayushchalise12@gmail.com>
2023-11-23 22:54:28 +0100

    Controllers and routes : Updated

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,3 +1,5 @@
+const asyncHandler = require("express-async-handler") ;
+
 //@desc Register a user
 //@route POST/api/users/register
 //@access public
@@ -5,5 +7,20 @@ const registerUser = asyncHandler(async(req, res) => {
   res.json({message: "Register the user"})
   });
 
-  module.exports= {registerUser};
+  //@descLogin a user
+//@route POST/api/users/Login
+//@access public
+const loginUser = asyncHandler(async (req, res)=>{
+    res.json({message: "login user"});
+});
+
+    //@desc Currnet user info
+//@route POST/api/users/register
+//@access private
+const currentUser = asyncHandler(async(req, res)=>{
+    res.json({message: "Current user information"});
+});
+
+  module.exports= {registerUser,loginUser,currentUser   };
+
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,12 @@
 const express = require("express");
+const { registerUser, loginUser, currentUser } = require("../controllers/userControllers");
 
 const router = express.Router();
 
-router.post("/register", (req, res)=>{
-    res.json({message: "Regster the user"});
-});
+router.post("/register", registerUser);
 
-router.post("/login", (req, res)=>{
-    res.json({message: "login user"});
-});
+router.post("/login",loginUser );
 
-router.post("/current", (req, res)=>{
-    res.json({message: "Current user information"});
-});
+router.get("/current",currentUser);
 
 module.exports = router;
